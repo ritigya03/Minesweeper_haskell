56f691e258be398702e5e016d92f6b8393eb687f
Kriti <kritic2905@gmail.com>
2024-07-01 23:04:31 +0530

    colours changed in Main.hs for testing

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ main = do
   set window [windowTitle := "Hello, It's Kriti!", containerBorderWidth := 50, windowDefaultWidth := 400, windowDefaultHeight := 400]
 
   -- Create a GdkColor for the desired background color
-  let bgColor = Color 0 235 6555  -- RGB values (0-65535 scale), here it's a light blue color
+  let bgColor = Color 65535 0 65535  -- RGB values (0-65535 scale), here it's a light blue color
 
   -- Modify the background color of the window
   widgetModifyBg window StateNormal bgColor
