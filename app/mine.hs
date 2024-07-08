f5a264be42775721713e4d979146889250b1f809
Kriti <kritic2905@gmail.com>
2024-07-08 19:52:21 +0530

    added function 'getNeighbours' to aid in recursively opening cells when an empty cell is clicked

--- app/mine.hs
+++ app/mine.hs
@@ -1,52 +1,62 @@
 import System.Random (randomRIO)
 import Control.Monad (replicateM)
 type Grid = [[Cell]]
-data Cell = Mine | Empty | Revealed Int deriving (Show, Eq)     
+data Cell = Mine | Empty | Revealed Int deriving (Show, Eq)
 
 
-initializeGrid :: Int -> Int -> IO Grid
+initializeGrid :: Int -> Int -> IO Grid 
 initializeGrid rows cols = return $ replicate rows (replicate cols Empty)
- 
- 
+
 placeMines :: Grid -> Int -> IO Grid
 placeMines grid numMines = do
     let rows = length grid
     let cols = length (head grid)
     minePositions <- replicateM numMines $ randomRIO ((0, 0), (rows - 1, cols - 1))
     return $ foldl (\g (r, c) -> placeMine g r c) grid minePositions
- 
- 
+
 placeMine :: Grid -> Int -> Int -> Grid
 placeMine grid row col = take row grid ++ [take col (grid !! row) ++ [Mine] ++ drop (col + 1) (grid !! row)] ++ drop (row + 1) grid
- 
- 
-printGrid :: Grid -> IO ()
+
+printGrid :: Grid -> IO()
 printGrid grid = mapM_ (putStrLn . concatMap showCell) grid
-  where
+    where
         showCell Mine = "💣 "
         showCell (Revealed 0) = "🍀 "
         showCell (Revealed n) = show n ++ " "
         showCell Empty = "🐥 "
-    
+
 setLevel :: String -> [Int]
 setLevel level = case level of
     "B" -> [8, 8, 10]
     "I" -> [16, 16, 40]
     "E" -> [30, 16, 99]
 
-countMines :: Grid -> Int -> Int -> Int 
-countMines grid row col = length[() | (r, c) <- neighbors, isMine r c] 
+countMines :: Grid -> Int -> Int -> Int
+countMines grid row col = length[() | (r, c) <- neighbours, isMine r c]
+    where 
+        -- rows = length grid
+        -- cols = length (head grid)
+        -- neighbours = [(r, c) | r <- [row - 1..row + 1], c <- [col - 1..col + 1], r >= 0, c >= 0, r < rows, c < cols, (r, c) /= (row, col)]
+        neighbours = getNeighbours grid row col
+        isMine r c = grid !! r !! c == Mine
+
+getNeighbours :: Grid -> Int -> Int -> [(Int, Int)]
+getNeighbours grid row col = [ (r, c) | r <- [row - 1..row + 1], c <- [col - 1..col + 1], r >= 0, c >= 0, r < rows, c < cols, (r, c) /= (row, col) ] 
     where
-    rows = length grid
-    cols = length(head grid)
-    neighbors = [ (r, c) | r <- [row - 1..row + 1], c <- [col - 1..col + 1], r >= 0, c >= 0, r < rows, c < cols, (r, c) /= (row, col) ] 
-    isMine r c = grid !!r !!c == Mine
+        rows = length grid
+        cols = length (head grid)
 
 revealCell :: Grid -> Int -> Int -> Grid
-revealCell grid row col =
+revealCell grid row col = 
     case grid !!row !!col of
-    Empty -> take row grid++ [take col(grid !!row)++ [Revealed(countMines grid row col)]++ drop(col + 1)(grid !!row)]++ drop(row + 1) grid
- 
+    Empty
+            -> take row grid
+                 ++
+                   [take col (grid !! row)
+                      ++
+                        [Revealed (countMines grid row col)]
+                          ++ drop (col + 1) (grid !! row)]
+                     ++ drop (row + 1) grid
 main :: IO ()
 main = do
     -- let rows = 5
@@ -59,5 +69,5 @@ main = do
     gridWithMines <- placeMines grid numMines
     printGrid gridWithMines
     putStrLn ""
-    let new = revealCell gridWithMines 3 9
+    let new = revealCell gridWithMines 6 1
     printGrid new
