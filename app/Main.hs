d820cfe18f39d7bddeda2418549381ee24d98a25
xmwriam <eqbalmariam@gmail.com>
2024-07-15 14:49:00 +0530

    fix: index too large error

--- app/Main.hs
+++ app/Main.hs
@@ -51,20 +51,20 @@ main = do
     let buttonRefs = [((i, j), btn) | ((i, j), btn) <- zip [(i, j) | i <- [0..rows-1], j <- [0..cols-1]] buttons]
 
     mines <- newIORef (replicate rows (replicate cols False))
-    mapM_ (\((i, j), btn) -> on btn buttonActivated (tileClicked mines buttonRefs (i, j))) buttonRefs
+    mapM_ (\((i, j), btn) -> on btn buttonActivated (tileClicked rows cols mines buttonRefs (i, j))) buttonRefs
     containerAdd window grid
     on window objectDestroy mainQuit
     widgetShowAll window
     mainGUI
 
-tileClicked :: IORef [[Bool]] -> [((Int, Int), Button)] -> (Int, Int) -> IO ()
-tileClicked mines buttons (i, j) = do
+tileClicked :: Int -> Int -> IORef [[Bool]] -> [((Int, Int), Button)] -> (Int, Int) -> IO ()
+tileClicked rows cols mines buttons (i, j) = do
     mineField <- readIORef mines
     if mineField !! i !! j
     then putStrLn "Game Over!"
     else do
         -- Update the button label
-        let neighbors = [(i+di, j+dj) | di <- [-1..1], dj <- [-1..1], i+di >= 0, i+di < 10, j+dj >= 0, j+dj < 10]
+        let neighbors = [(i+di, j+dj) | di <- [-1..1], dj <- [-1..1], i+di >= 0, i+di < rows, j+dj >= 0, j+dj < cols]
         let mineCount = length $ filter id [mineField !! x !! y | (x, y) <- neighbors]
         let Just btn = lookup (i, j) buttons
         set btn [buttonLabel := show mineCount]
